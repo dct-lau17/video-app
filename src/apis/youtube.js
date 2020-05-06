ac34868fb47649d1382827935c2686c1052d6627
dct-lau17 <daniel.lau1@tesco.com>
2020-05-06 23:06:07 +0100

    rename API key

--- src/apis/youtube.js
+++ src/apis/youtube.js
@@ -1,12 +1,12 @@
 import axios from "axios";
-const KEY = "AIzaSyCVzOatyTl-T9Ii0LRfFTMsSC_74yleeTs";
+const API_KEY = process.env.YOUTUBE_API_KEY;
 export default axios.create({
   baseURL: "https://www.googleapis.com/youtube/v3",
   params: {
       part: 'snippet',
       maxResults: 5,
       type: 'video',
-      key: KEY
+      key: API_KEY
       
   }
 });
