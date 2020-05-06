711a695d0720b0da3fa6947b4ec2c58e2f3f2972
dct-lau17 <daniel.lau1@tesco.com>
2020-05-06 23:30:35 +0100

    revert changes

--- src/apis/youtube.js
+++ src/apis/youtube.js
@@ -1,5 +1,5 @@
 import axios from "axios";
-const API_KEY = process.env.YOUTUBE_API_KEY;
+const API_KEY = "AIzaSyCVzOatyTl-T9Ii0LRfFTMsSC_74yleeTs";
 export default axios.create({
   baseURL: "https://www.googleapis.com/youtube/v3",
   params: {
