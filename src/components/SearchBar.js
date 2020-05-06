aa42c56f98622c8028df198c51ae2f37adcaf3cc
Daniel Lau <daniel.lau1@tesco.com>
2020-05-06 16:47:36 +0100

    Add ui semantic style sheet and search bar input

--- src/components/SearchBar.js
+++ src/components/SearchBar.js
@@ -1,10 +1,26 @@
-import React from 'react';
+import React from "react";
 
-class SearchBar extends React.Component{
+class SearchBar extends React.Component {
+  state = { term: "type something" };
 
-    render(){
-        return <div> Search Bar</div>
-    }
+ 
+
+  render() {
+    return (
+      <div className="search-bar ui segment">
+        <form className="ui form">
+          <div className="field">
+            <label>Video Search</label>
+            <input
+              type="text"
+              value={this.state.term}
+              
+            />
+          </div>
+        </form>
+      </div>
+    );
+  }
 }
 
 export default SearchBar;
