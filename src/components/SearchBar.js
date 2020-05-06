05062eb29b8722496e6afad93d76cdccf703f872
Daniel Lau <daniel.lau1@tesco.com>
2020-05-06 16:54:03 +0100

    search bar state and event handlers

--- src/components/SearchBar.js
+++ src/components/SearchBar.js
@@ -1,20 +1,27 @@
 import React from "react";
 
 class SearchBar extends React.Component {
-  state = { term: "type something" };
+  state = { term: "" };
 
- 
+  onInputChange = (event) => {
+    this.setState({term: event.target.value})
+  };
+
+  onFormSubmit = event => {
+      event.preventDefault();
+      console.log(this.state.term);
+  };
 
   render() {
     return (
       <div className="search-bar ui segment">
-        <form className="ui form">
+        <form className="ui form" onSubmit={this.onFormSubmit}>
           <div className="field">
             <label>Video Search</label>
             <input
               type="text"
               value={this.state.term}
-              
+              onChange={e => {this.onInputChange(e)}}
             />
           </div>
         </form>
